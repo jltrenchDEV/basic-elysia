44f27b63eb311d2aa6c9532821125283a9487906
jltrenchDEV <jltrench.dev@gmail.com>
2024-01-25 16:14:45 -0300

    docker env add

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /usr/src/app
 COPY package*.json bun.lockb ./
 RUN bun install
 COPY . .
+COPY .env ./
 
 ENV NODE_ENV production
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { api } from './controllers/index';
-import { Elysia, t } from 'elysia';
+import { Elysia } from 'elysia';
 
-const app = new Elysia().use(api).listen(3000);
+const app = new Elysia().use(api).listen(3001);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
